07e472cb0eec4120cc79e2e1538a33161ae89aaa
Karol Moroz <karol@moroz.dev>
2024-05-12 10:20:32 +0900

    Add Copy to Clipboard button

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,4 @@
+document.querySelectorAll("pre.chroma").forEach((block) => {
+  console.log(block);
+});
 
--- vite/src/main.ts
+++ vite/src/main.ts
@@ -1,1 +1,20 @@
 import "./main.scss";
+
+document.querySelectorAll("pre.chroma").forEach((block) => {
+  const button = document.createElement("button");
+  button.setAttribute("type", "button");
+  button.className = "button copy-to-clipboard";
+  button.textContent = "Copy to clipboard";
+
+  button.addEventListener("click", () => {
+    const lines = [...block.querySelectorAll(`span.cl`)]
+      .map((line) => line.textContent)
+      .join("")
+      .trim();
+
+    navigator.clipboard.writeText(lines);
+  });
+
+  const container = block.parentElement;
+  container?.appendChild(button);
+});
